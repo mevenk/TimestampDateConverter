631f2b1a84631d64e8821cb52402fbbea27ce683
Venkatesh Kolisetty <venky.svtv@live.com>
2017-07-18 23:24:48 +0530

    Running Clock Added

--- src/main/java/mevenk/timestampdateconverter/utility/TimestampDateConverter.java
+++ src/main/java/mevenk/timestampdateconverter/utility/TimestampDateConverter.java
@@ -1,21 +1,30 @@
 package mevenk.timestampdateconverter.utility;
 
 import java.awt.Button;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.Window.Type;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.SwingWorker;
+import javax.swing.border.EtchedBorder;
 
 /**
  * The Class TimestampDateConverter.
@@ -40,8 +49,19 @@ public class TimestampDateConverter {
 	/** The copy date to clipboard button. */
 	private JButton copyDateToClipboardButton;
 
+	private RunningClock runningClock = new RunningClock();
+
+	private JPopupMenu contextMenu;
+	private JPopupMenu contextMenu2;
+
+	private JLabel lblRunningclock;
+
 	/** The Constant lineSeparator. */
 	private final static String lineSeparator = System.lineSeparator();
+
+	private static final SimpleDateFormat SIMPLE_DATE_FORMAT_RUNNING_CLOCK = new SimpleDateFormat(
+			"dd/MM/yyyy HH:mm:ss,SSS");
+	private JLabel labelRunningClockTimestamp;
 
 	/**
 	 * Launch the application.
@@ -68,6 +88,7 @@ public class TimestampDateConverter {
 	 */
 	public TimestampDateConverter() {
 		initialize();
+		runningClock.execute();
 	}
 
 	/**
@@ -78,11 +99,18 @@ public class TimestampDateConverter {
 		timestampDateConverterFrame.setTitle("Timestamp/Date Converter");
 		timestampDateConverterFrame.getContentPane().setEnabled(false);
 		timestampDateConverterFrame.setResizable(false);
+		timestampDateConverterFrame.setIconImage(Toolkit.getDefaultToolkit()
+				.getImage(TimestampDateConverter.class.getResource("/mevenk/image/mevenkGitHubLogo.png")));
 		timestampDateConverterFrame.setType(Type.UTILITY);
 		timestampDateConverterFrame.setAlwaysOnTop(true);
-		timestampDateConverterFrame.setBounds(100, 100, 500, 300);
+		timestampDateConverterFrame.setBounds(100, 100, 565, 300);
 		timestampDateConverterFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		timestampDateConverterFrame.getContentPane().setLayout(null);
+
+		Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
+		timestampDateConverterFrame.setLocation(
+				screenDimension.width / 2 - timestampDateConverterFrame.getSize().width / 2,
+				screenDimension.height / 2 - timestampDateConverterFrame.getSize().height / 2);
 
 		timestampTextField = new JTextField();
 		timestampTextField.setToolTipText("Timestamp in millis");
@@ -93,7 +121,7 @@ public class TimestampDateConverter {
 		simpleDateFormatTextField = new JTextField();
 		simpleDateFormatTextField.setToolTipText("Date + Time  Recommended");
 		simpleDateFormatTextField.setColumns(10);
-		simpleDateFormatTextField.setBounds(151, 40, 175, 40);
+		simpleDateFormatTextField.setBounds(189, 42, 175, 40);
 		timestampDateConverterFrame.getContentPane().add(simpleDateFormatTextField);
 
 		Button toDateButton = new Button("To Date  >>");
@@ -120,7 +148,7 @@ public class TimestampDateConverter {
 			}
 
 		});
-		toDateButton.setBounds(175, 100, 125, 50);
+		toDateButton.setBounds(211, 100, 125, 50);
 		timestampDateConverterFrame.getContentPane().add(toDateButton);
 
 		toTimestampButton = new Button("<<  To timestamp");
@@ -146,13 +174,13 @@ public class TimestampDateConverter {
 
 			}
 		});
-		toTimestampButton.setBounds(175, 165, 125, 50);
+		toTimestampButton.setBounds(211, 156, 125, 50);
 		timestampDateConverterFrame.getContentPane().add(toTimestampButton);
 
 		dateTextField = new JTextField();
 		dateTextField.setToolTipText("Date");
 		dateTextField.setColumns(10);
-		dateTextField.setBounds(334, 135, 150, 40);
+		dateTextField.setBounds(399, 135, 150, 40);
 		timestampDateConverterFrame.getContentPane().add(dateTextField);
 
 		JButton copyTimestampToClipboardButton = new JButton("Copy to Clipboard");
@@ -167,7 +195,7 @@ public class TimestampDateConverter {
 
 			}
 		});
-		copyTimestampToClipboardButton.setBounds(20, 200, 125, 30);
+		copyTimestampToClipboardButton.setBounds(10, 200, 150, 30);
 		timestampDateConverterFrame.getContentPane().add(copyTimestampToClipboardButton);
 
 		copyDateToClipboardButton = new JButton("Copy to Clipboard");
@@ -180,12 +208,12 @@ public class TimestampDateConverter {
 
 			}
 		});
-		copyDateToClipboardButton.setBounds(345, 200, 125, 30);
+		copyDateToClipboardButton.setBounds(399, 200, 150, 30);
 		timestampDateConverterFrame.getContentPane().add(copyDateToClipboardButton);
 
 		JLabel labelDateFormat = new JLabel("Date Format");
 		labelDateFormat.setHorizontalAlignment(SwingConstants.CENTER);
-		labelDateFormat.setBounds(200, 11, 75, 20);
+		labelDateFormat.setBounds(230, 11, 75, 20);
 		timestampDateConverterFrame.getContentPane().add(labelDateFormat);
 
 		JLabel lblTimestamp = new JLabel("Timestamp(millis)");
@@ -195,13 +223,105 @@ public class TimestampDateConverter {
 
 		JLabel lblDate = new JLabel("DATE");
 		lblDate.setHorizontalAlignment(SwingConstants.CENTER);
-		lblDate.setBounds(370, 100, 75, 20);
+		lblDate.setBounds(435, 100, 75, 20);
 		timestampDateConverterFrame.getContentPane().add(lblDate);
 
-		Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
-		timestampDateConverterFrame.setLocation(
-				screenDimension.width / 2 - timestampDateConverterFrame.getSize().width / 2,
-				screenDimension.height / 2 - timestampDateConverterFrame.getSize().height / 2);
+		lblRunningclock = new JLabel("");
+		lblRunningclock.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				lblRunningclock.setBackground(Color.GRAY);
+				runningClock.cancel(false);
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				if (contextMenu.isShowing()) {
+					return;
+				}
+				runningClock = new RunningClock();
+				runningClock.execute();
+				lblRunningclock.setBackground(null);
+			}
+		});
+		lblRunningclock.setBounds(370, 25, 175, 40);
+		lblRunningclock.setHorizontalAlignment(SwingConstants.CENTER);
+		lblRunningclock.setFont(new Font("Trebuchet MS", Font.BOLD, 14));
+		lblRunningclock.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		timestampDateConverterFrame.getContentPane().add(lblRunningclock);
+
+		contextMenu = new JPopupMenu();
+		addPopup(lblRunningclock, contextMenu);
+
+		JButton copyButton = new JButton("Copy");
+		copyButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				String lblRunningclockText = lblRunningclock.getText();
+				StringSelection packageNameTextFieldTextStringSelection = new StringSelection(lblRunningclockText);
+				Toolkit.getDefaultToolkit().getSystemClipboard().setContents(packageNameTextFieldTextStringSelection,
+						null);
+
+				if (contextMenu.isShowing()) {
+					runningClock = new RunningClock();
+					runningClock.execute();
+					lblRunningclock.setBackground(null);
+					contextMenu.setVisible(false);
+				}
+			}
+		});
+		contextMenu.add(copyButton);
+
+		labelRunningClockTimestamp = new JLabel("");
+		labelRunningClockTimestamp.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				labelRunningClockTimestamp.setBackground(Color.GRAY);
+				runningClock.cancel(false);
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				if (contextMenu2.isShowing()) {
+					return;
+				}
+				runningClock = new RunningClock();
+				runningClock.execute();
+				labelRunningClockTimestamp.setBackground(null);
+			}
+		});
+		labelRunningClockTimestamp.setHorizontalAlignment(SwingConstants.CENTER);
+		labelRunningClockTimestamp.setFont(new Font("Trebuchet MS", Font.BOLD, 14));
+		labelRunningClockTimestamp.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		labelRunningClockTimestamp.setBounds(10, 25, 175, 40);
+		timestampDateConverterFrame.getContentPane().add(labelRunningClockTimestamp);
+
+		contextMenu2 = new JPopupMenu();
+		addPopup(labelRunningClockTimestamp, contextMenu2);
+
+		JButton copyButton2 = new JButton("Copy");
+		copyButton2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				String lblRunningclockText = labelRunningClockTimestamp.getText();
+				StringSelection labelRunningClockTimestampTextStringSelection = new StringSelection(
+						lblRunningclockText);
+				Toolkit.getDefaultToolkit().getSystemClipboard()
+						.setContents(labelRunningClockTimestampTextStringSelection, null);
+
+				if (contextMenu2.isShowing()) {
+					runningClock = new RunningClock();
+					runningClock.execute();
+					labelRunningClockTimestamp.setBackground(null);
+					contextMenu2.setVisible(false);
+				}
+			}
+		});
+		contextMenu2.add(copyButton2);
 
 	}
 
@@ -220,4 +340,46 @@ public class TimestampDateConverter {
 		return true;
 	}
 
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
+	}
+
+	class RunningClock extends SwingWorker<Void, Void> {
+
+		boolean keepClockRunning = true;
+
+		@Override
+		protected Void doInBackground() throws Exception {
+			Date date = new Date();
+
+			while (keepClockRunning) {
+				date = new Date();
+				lblRunningclock.setText(SIMPLE_DATE_FORMAT_RUNNING_CLOCK.format(date));
+				labelRunningClockTimestamp.setText(String.valueOf(date.getTime()));
+			}
+			return null;
+		}
+
+		@Override
+		public void done() {
+			keepClockRunning = false;
+		}
+
+	}
 }
